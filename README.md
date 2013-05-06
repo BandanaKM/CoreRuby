d79fbd9a1dd7315e4c56aaf27e318a6bcb5fcc79
cokeefe <chaemobile@gmail.com>
2013-05-06 10:26:28 -0400

    update README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ruby Learning : Core Ruby PORPC101
+####by Satish Talim
+The [*Core Ruby*](http://rubylearning.com/satishtalim/tutorial.html) class is a beginner level course covering the essential features of the ruby language. Each week includes a number of exercises, extended challenges, tutorials, and a weekly quizes. 
+
+Private tutors will evaluate student exercises and provide personal feedback, typically within 24 hours. Additional mentor feedback is available through impromptu google hangouts, pair programming, e-mail Q&A, and irc.
+
+The dedicated mentor to the 41st Core Ruby class is Victor Goff III.
--- week4/W4E3.rb
+++ week4/W4E3.rb
@@ -42,9 +42,14 @@ call_granny.talk_to_granny
 Can you explain why you have two files, and what it is doing for you, why it is a good idea to sometimes break up your code into files?
 Also, can you explain what is going on with the talk_to_granny method in both files? Why it is in both, what the disadvantage or advantage may be?  
 
-By creating a new file, I was able to modify the same class method without changing the base/original file's contents. 
+-By creating a new file, I was able to modify the same class method without changing the base/original file's contents. 
 I suppose having two definitions of the same class method could start to get confusing. 
 If one the new file's intent were to add or expand additional class methods to the same class, it might be different. 
 I'm sure that ruby gems are different, but it seems like they are providing addiontional functions and resources, and not necessarily redefining any of the core-library functions that are pre-exisiting 
 
+- OK, I see now. But now you have a dependency. You must require the first file, and then the second file, otherwise things may be surprising, right?
+
+So it is probably better to have the program in the same file right now. The fact that you overwrite one your methods may mean you need to simplify it. Don't Repeat Yourself (DRY) right?
+
+Hey, just remembered, we have not gotten together in a hangout yet. You up for it? You were ready to do so 4 weeks ago!
 =end
