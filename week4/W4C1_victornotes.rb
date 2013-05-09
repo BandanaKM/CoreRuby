eacdbcb61c5a6a9347b18a3d1d5b9491c77063a9
cokeefe <chaemobile@gmail.com>
2013-05-09 10:11:34 -0400

    filerename

--- week4/W4C1_victornotes.rb
+++ week4/W4C1_victornotes.rb
@@ -7,36 +7,51 @@ d = Dog.new('Leo')
 =end
 
 class Dog
-	attr_accessor :name, :tricks
+  attr_accessor :name, :tricks
 
-	def initialize(name)
-      @name = name 
-      @tricks = {}
-	end
-  
-  def teach_trick(sym, &block)
-    @tricks[sym] = block.call
-    
+  def initialize(name)
+    @name = name 
+    @tricks = {}
   end
-  
-  def method_missing(instance_id)
-      "#{@name} doesn't know how to #{instance_id}!"
+
+  def teach_trick(sym, &block)
+    @tricks[sym] = block
+  end
+
+  def respond_to_missing?(meth, *args )
+    !!@tricks[meth]
+  end
+
+  def method_missing(meth, *args, &blk)
+    if @tricks[meth]
+      @tricks[meth].call(args)
+    else
+      "#{@name} doesn't know how to #{meth}!"
+    end
+  end
+
+  def list_tricks
+    @tricks.inspect
   end
 end
 
 d = Dog.new('Leo')
-puts d.teach_trick(:dance) {"#{@name} is dancing!"}
-puts d.dance
+puts d.respond_to_missing?(:hula)
+puts d.hula
 
-puts d.teach_trick(:poo) { "#{@name} is a smelly doggy!" }
+puts "=" * 30
+d.teach_trick(:dance) {|color| "#{d.name} is dancing #{color} dance!"}
+puts d.dance("blue")
+
+d.teach_trick(:poo) { "#{d.name} is a smelly doggy!" }
 puts d.poo
-
+puts d.list_tricks
 =begin
-  
+
 You can store the Proc in an instance variable that contains a hash of tricks. That might be one option.
 Also, there are things that let you evaluate strings into code... not surprisingly, they have names such as eval, instance_eval, etc.
 When you use a form of 'eval', it makes real methods, and so will utilize respond_to? method. With the tricks stored in a hash idea, you may use something like method missing and write your own responds_to? method to accommodate these things.
 Many choices, which is perhaps part of the problem with solving this challenge, but it also means you can determine the best approach of the given situation.
 I hope that helps.
-  
+
 =end
